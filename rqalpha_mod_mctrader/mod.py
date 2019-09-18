be42fc3c80848d67788c31b2ed2ddafe28c3d873
match5 <match5@qq.com>
2019-09-19 00:22:03 +0800

    redirect logs to file

--- rqalpha_mod_mctrader/mod.py
+++ rqalpha_mod_mctrader/mod.py
@@ -5,12 +5,19 @@
 from rqalpha.interface import AbstractMod
 from rqalpha.data.data_proxy import DataProxy
 
+from rqalpha.utils.logger import (
+    user_std_handler_log_formatter ,system_log, 
+    basic_system_log, std_log, user_log, user_system_log
+)
+
 from .data_source.tusharepro import TushareProDataSource
 from .event_source import McTraderEventSource
 from .broker.agent_broker import AgentBroker
 from .price_board import McTraderPriceBoard
 
 import tushare as ts
+
+from logbook import FileHandler
 
 class McTraderMod(AbstractMod):
     def __init__(self):
@@ -25,6 +32,17 @@ class McTraderMod(AbstractMod):
         env.set_data_proxy(DataProxy(env.data_source, env.price_board))
         env.set_event_source(McTraderEventSource(env, mod_config))
         env.set_broker(AgentBroker(env, mod_config))
+        if mod_config.log_file:
+            user_log.handlers = []
+            user_system_log.handlers = []
+            user_handler = FileHandler(mod_config.log_file, bubble=True)
+            user_handler.formatter = user_std_handler_log_formatter
+            if not env.config.extra.user_log_disabled:
+                user_log.handlers.append(user_handler)
+            if not env.config.extra.user_system_log_disabled:
+                user_system_log.handlers.append(user_handler)
+            system_log.handlers = [FileHandler(mod_config.log_file, mode='a', bubble=True)]
+            basic_system_log.handlers = [FileHandler(mod_config.log_file, bubble=True)]
 
     def tear_down(self, code, exception=None):
         pass
